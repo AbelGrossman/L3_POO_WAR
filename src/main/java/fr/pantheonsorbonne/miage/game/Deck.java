09dda35c0955c5f477d50d04152cef99ee4471d9
Noam Boulze <noamboulze@gmail.com>
2023-11-29 12:13:38 +0100

    modified:   src/main/java/fr/pantheonsorbonne/miage/game/Deck.java

--- src/main/java/fr/pantheonsorbonne/miage/game/Deck.java
+++ src/main/java/fr/pantheonsorbonne/miage/game/Deck.java
@@ -4,25 +4,31 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+//Classe Deck (les cartes du joueur)
 public class Deck {
 
+    //Liste permettant de stocker les cartes dans le deck complet du jeu
     List<Carte> deckComplet = new ArrayList<>();
 
+    //Constrcuteur de la classe deck
     public Deck() {
+        //appel à la méthode ajouter au deck définit juste en dessous
         ajouterAuDeck();
     }
 
     public void ajouterAuDeck() {
         String[] typesDeCartes = { "Trefle", "Pique", "Carreau", "Coeur" };
-        String type = null;
+        String type = null;     //couleur de la carte
         String nom = null;
-        boolean bout = false;
+        boolean bout = false;   //true si la carte est un bout (petit, 21 ou excuse)
         int points = 1;
+        //Boucles pour ajouter toutes les cartes de chaque type dans le deck
         for (int i = 0; i < 4; i++) {
             type = typesDeCartes[i];
             points = 1;
             for (int j = 1; j < 15; j++) {
                 switch (j) {
+                    // Assigner des noms et points spéciaux aux têtes.
                     case 11:
                         nom = "Valet de " + type;
                         points = 2;
@@ -43,10 +49,12 @@ public class Deck {
                         nom = j + " de " + type;
                         break;
                 }
+                // Ajouter la carte créée au deck.
                 Carte nouvelleCarte = new Carte(type, j, nom, bout, points);
                 deckComplet.add(nouvelleCarte);
             }
         }
+        //Ajout des atouts au deck
         for (int i = 1; i < 22; i++) {
             bout = false;
             points = 1;
@@ -58,6 +66,7 @@ public class Deck {
             Carte nouvelleCarte = new Carte("Atout", i, nom, bout, points);
             deckComplet.add(nouvelleCarte);
         }
+        // Ajouter la carte Excuse au deck.
         Carte nouvelleCarte = new Carte("Excuse", 22, "Excuse", bout, 5);
         deckComplet.add(nouvelleCarte);
     }
