16b5f9887dcda3981dfa91c6c2ecfaa7da135cd5
Noam Boulze <noamboulze@gmail.com>
2023-11-29 11:24:52 +0100

    modified:   src/main/java/fr/pantheonsorbonne/miage/game/Application.java 	modified:   src/main/java/fr/pantheonsorbonne/miage/game/Carte.java

--- src/main/java/fr/pantheonsorbonne/miage/game/Application.java
+++ src/main/java/fr/pantheonsorbonne/miage/game/Application.java
@@ -2,9 +2,12 @@ package fr.pantheonsorbonne.miage.game;
 
 import java.util.List;
 
+// Classe principale, c'est ici que le programme commence son exécution et lance donc une partie
 public class Application {
     public static void main(String... args) {
+        //Création d'un deck
         Deck deckTarot = new Deck();
+        //Création d'une nouvelle partie
         Partie partie = new Partie(3, deckTarot, 9); //(nb_joueur, deck, nb_manches)
     }
 }
