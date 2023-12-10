dbae4fa0cd148c0c3944c1b289bd04c26a41cd44
Noam Boulze <noamboulze@gmail.com>
2023-12-10 23:47:14 +0100

    comment Joueur 	modified:   src/main/java/fr/pantheonsorbonne/miage/game/Joueur.java

--- src/main/java/fr/pantheonsorbonne/miage/game/Joueur.java
+++ src/main/java/fr/pantheonsorbonne/miage/game/Joueur.java
@@ -90,34 +90,37 @@ public class Joueur {
 
     //Méthode vérifiant la validité d'une carte
     private boolean isValidCard(Carte carteJouee, String couleurDemandee, Carte carteGagnante) {
+        //verifie si une couleur est demandée
         if (couleurDemandee != null) {
             if (carteJouee.typeCarte.equals("Excuse")) {
                 System.out.println("case 1");
                 return true;
             }
+            //Si la carte est de la couleur demandée et n'est pas un atout alors elle est valide
             if (carteJouee.typeCarte.equals(couleurDemandee) && !carteJouee.typeCarte.equals("Atout")) {
                 System.out.println("case 2");
                 return true;
             }
-
+            //Si la carte est un atout mais pas de la couleur demandée et si le joueur n'a pas de cartes de la couleur demandée alors elle est valide
             if (carteJouee.typeCarte.equals("Atout") && !carteJouee.typeCarte.equals(couleurDemandee)
                     && nombreCarteCouleurDansMain.get(couleurDemandee) == 0) {
                 System.out.println("case 3");
                 return true;
             }
-
+             // Si la carte est de la couleur demandée et un atout et a une valeur supérieure à la carte gagnante alors elle est valide
             if (carteJouee.typeCarte.equals(couleurDemandee) && carteJouee.typeCarte.equals("Atout")
                     && (carteJouee.valeurCarte > carteGagnante.valeurCarte
                             || nombreAtoutsSups == 0)) {
                 System.out.println("case 4");
                 return true;
             }
-
+            //Si la carte n'est pas de la couleur demandé et que la couleur demandée est un atout et que le joueur n'a pas de cartes de la couleur demandée alors elle est valide
             if (!carteJouee.typeCarte.equals(couleurDemandee) && couleurDemandee.equals("Atout")
                     && nombreCarteCouleurDansMain.get(couleurDemandee) == 0) {
                 System.out.println("case 5");
                 return true;
             }
+            // Si la couleur demandée n'est pas un atout, que la carte n'est pas de la couleur demandée et que le joueur n'a pas de cartes ni d'atouts ni de la couleur demandée alors elle est valide
             if (!couleurDemandee.equals("Atout") && (!carteJouee.typeCarte.equals(couleurDemandee)
                     && nombreCarteCouleurDansMain.get("Atout") == 0
                     && nombreCarteCouleurDansMain.get(couleurDemandee) == 0)) {
@@ -125,11 +128,8 @@ public class Joueur {
                 return true;
 
             }
-        } else
-
-        {
-            // Handle the case where couleurDemandee is null
-            return true; // Assuming any card is valid in this case
+        } else{
+            return true; 
         }
 
         return false;
